84c9ef329a97441dc2b21d7ff33fc647d673e412
Jason K. Moore <moorepants@gmail.com>
2022-03-23 14:51:10 +0100

    Added ability to force overwrites on dreamhost.

--- dreamobject.py
+++ dreamobject.py
@@ -7,26 +7,42 @@ import os
 
 import boto
 
-accepted_file_exts = ['.pdf', '.png', '.gif', '.mp4', '.jpg']
-
-conn = boto.connect_s3(host='objects-us-east-1.dream.io')
-
-bucket = conn.get_bucket('mechmotum')
-
+accepted_file_exts = ['.pdf', '.png', '.gif', '.mp4', '.jpg', '.svg']
+BUCKET_NAME = 'mechmotum'
+HOST = 'objects-us-east-1.dream.io'
 assets_dir = 'assets'
 
-files = os.listdir(assets_dir)
 
-for fname in files:
-    if os.path.splitext(fname)[-1] in accepted_file_exts:
-        key = boto.s3.key.Key(bucket, fname)
-        if key.exists():
-            print('Skipping: {} (already present in the bucket)'.format(fname))
+def upload(overwrite=False):
+
+    conn = boto.connect_s3(host=HOST)
+    bucket = conn.get_bucket(BUCKET_NAME)
+
+    files = os.listdir(assets_dir)
+
+    for fname in files:
+        if os.path.splitext(fname)[-1] in accepted_file_exts:
+            key = boto.s3.key.Key(bucket, fname)
+            if key.exists() and overwrite is False:
+                msg = 'Skipping: {} (already present in the bucket)'
+                print(msg.format(fname))
+            else:
+                print('Uploading: {}'.format(fname))
+                key.set_contents_from_filename(os.path.join(assets_dir, fname))
         else:
-            print('Uploading: {}'.format(fname))
-            key.set_contents_from_filename(os.path.join(assets_dir, fname))
-    else:
-        print('Skipping: {}'.format(fname))
+            print('Skipping: {}'.format(fname))
 
-for o in bucket.list():
-    o.set_acl('public-read')
+    for o in bucket.list():
+        o.set_acl('public-read')
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Upload Files to Dreamhost.')
+    parser.add_argument('--overwrite', action='store_true',
+                        help="Forces files to be overwritten.")
+    args = parser.parse_args()
+
+    upload(overwrite=args.overwrite)
