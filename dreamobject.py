ecd315ef4aff5b76ba6f7c07ed760478ec3e44e1
Jason K. Moore <moorepants@gmail.com>
2025-11-17 11:00:29 +0100

    Updated to boto3 and the new dreamobjects location.

--- dreamobject.py
+++ dreamobject.py
@@ -1,39 +1,44 @@
-# Create a ~/.boto file with:
+# Create a ~/.aws/credentials file with:
 #[Credentials]
 #aws_access_key_id = <key>
 #aws_secret_access_key = <key>
 
 import os
 
-import boto
+import boto3
+from botocore.exceptions import ClientError
 
 accepted_file_exts = ['.pdf', '.png', '.gif', '.mp4', '.jpg', '.svg']
 BUCKET_NAME = 'mechmotum'
-HOST = 'objects-us-east-1.dream.io'
+HOST = 's3.us-east-005.dream.io'
 assets_dir = 'assets'
 
 
 def upload(overwrite=False):
 
-    conn = boto.connect_s3(host=HOST)
-    bucket = conn.get_bucket(BUCKET_NAME)
+    session = boto3.session.Session()
+    s3 = session.resource('s3', endpoint_url=f'https://{HOST}')
+    bucket = s3.Bucket(BUCKET_NAME)
 
     files = os.listdir(assets_dir)
 
     for fname in files:
         if os.path.splitext(fname)[-1] in accepted_file_exts:
-            key = boto.s3.key.Key(bucket, fname)
-            if key.exists() and overwrite is False:
-                msg = 'Skipping: {} (already present in the bucket)'
-                print(msg.format(fname))
-            else:
+            s3_object = bucket.Object(fname)
+            try:
+                s3_object.last_modified
+            except ClientError:
                 print('Uploading: {}'.format(fname))
-                key.set_contents_from_filename(os.path.join(assets_dir, fname))
-        else:
-            print('Skipping: {}'.format(fname))
-
-    for o in bucket.list():
-        o.set_acl('public-read')
+                s3_object.upload_file(os.path.join(assets_dir, fname))
+                s3_object.Acl().put(ACL='public-read')
+            else:
+                if overwrite is False:
+                    msg = 'Skipping: {} (already present in the bucket)'
+                    print(msg.format(fname))
+                else:
+                    print('Overwriting, uploading: {}'.format(fname))
+                    s3_object.upload_file(os.path.join(assets_dir, fname))
+                    s3_object.Acl().put(ACL='public-read')
 
 
 if __name__ == "__main__":
